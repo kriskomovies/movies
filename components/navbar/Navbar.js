32fc9442569005e0654218e3499427525591ae13
krizk0bg <145044722+krizk0bg@users.noreply.github.com>
2024-01-06 12:34:08 +0200

    fixed for phone

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -25,18 +25,29 @@ export default function Navbar() {
         <div className={styles.navbar}>
             <div className={styles.container}>
                 <div className={styles.leftSide}>
-                    <Link href="/" style={{margin: 0}} >
+                    <Link href="/" style={{margin: 0}}>
                         <img src={"/popflix-nobg-logo.png"} alt="alt nav" width={100} height={100}/>
                     </Link>
-                    <Link
-                        onClick={resetSearch} href={moviesURI} className={type === MOVIE ? styles.activeLink : '' }>Movies
-                    </Link>
-                    <Link
-                        onClick={resetSearch} href={seriesURI} className={type === SERIE ? styles.activeLink : ''}>Series
-                    </Link>
-                    <Link
-                        onClick={resetSearch} href={animesURI} className={type === ANIME ? styles.activeLink : ''}>Animes
-                    </Link>
+                    <div className={styles.navLinks}>
+                        <div>
+                            <Link
+                                onClick={resetSearch} href={moviesURI}
+                                className={type === MOVIE ? styles.activeLink : ''}>Movies
+                            </Link>
+                        </div>
+                        <div>
+                            <Link
+                                onClick={resetSearch} href={seriesURI}
+                                className={type === SERIE ? styles.activeLink : ''}>Series
+                            </Link>
+                        </div>
+                        <div>
+                            <Link
+                                onClick={resetSearch} href={animesURI}
+                                className={type === ANIME ? styles.activeLink : ''}>Animes
+                            </Link>
+                        </div>
+                    </div>
                 </div>
                 <div className={styles.dropDown}>
                     <GenresDropdown/>
