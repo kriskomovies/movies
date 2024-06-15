de63cc34c1163e46760b908eac834d4b2454b261
krizk0bg <145044722+krizk0bg@users.noreply.github.com>
2024-06-15 22:49:41 +0300

    on hove image fix

--- components/carousel/item/CarouselItem.js
+++ components/carousel/item/CarouselItem.js
@@ -1,11 +1,18 @@
-import Image from 'next/image'
-import styles from "./carouselItem.module.scss";
+import { useState } from 'react';
+import Image from 'next/image';
+import styles from './carouselItem.module.scss';
 
-export default function CarouselItem({movie, hasName}) {
-    const {poster, episode, episodeName, name, imdbRating} = movie;
+export default function CarouselItem({ movie, hasName }) {
+    const [isHovered, setIsHovered] = useState(false);
+    const { poster, episode, episodeName, name, imdbRating } = movie;
+
     return (
-        <div className={styles.container}>
-            <div className={styles.imageContainer}>
+        <div className={styles.container}
+             onMouseEnter={() => setIsHovered(true)}
+             onMouseLeave={() => setIsHovered(false)}>
+            <div
+                className={`${styles.imageContainer} ${isHovered ? styles.hovered : ''}`}
+            >
                 <Image
                     className={styles.img}
                     src={poster}
@@ -16,16 +23,15 @@ export default function CarouselItem({movie, hasName}) {
             </div>
             <div className={styles.carouselHoverContent}>
                 <div className={styles.hoverMovieName}>{name}</div>
-                <hr/>
+                <hr />
                 <div className={styles.hoverMovieRating}>
-                    Rating:
-                    <span>{imdbRating}</span>/10.0
+                    Rating: <span>{imdbRating}</span>/10.0
                 </div>
             </div>
             {hasName && (
                 <div className={styles.movieName}>
                     <div className={styles.episodeName}>
-                        <div className={styles.episodeParts}>Episode : {episode}</div>
+                        <div className={styles.episodeParts}>Episode: {episode}</div>
                         {episodeName && <div className={styles.episodeParts}>{episodeName}</div>}
                     </div>
                 </div>
